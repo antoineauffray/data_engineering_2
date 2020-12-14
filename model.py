41020ea799f9d5e8554b2830b7373b1f16e5db40
Solenya <antoinemarc.auffray@gmail.com>
2020-12-15 00:08:19 +0100

    deleting print in model.py

--- model.py
+++ model.py
@@ -29,8 +29,6 @@ def predict():
                 model = request.form['model']
             except:
                 model = "w2v"
-    
-            print("\n Model used :", model, '\n', file=sys.stderr)
         
             result = get_similar_tweets(word, model, nlp)
 
