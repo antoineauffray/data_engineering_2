c64721b4dd23c374f4c8cfee5e1885c06fa49df8
anna_fischmeister <anna.fischmeister@efrei.net>
2020-12-15 20:53:51 +0100

    modification

--- integration_test.py
+++ integration_test.py
@@ -14,15 +14,15 @@ class FlaskTests(unittest.TestCase):
     def test_page(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-
-    def test_model_d2v(self):
-        data = {'word_to_analyse':'election', 'model':'d2v'}
-        response = self.app.post('http://localhost:5000/', data=data)
-        #print(response.data)        
-        self.assertEqual(response.status_code,200)
     
     def test_model_w2v(self):
         data = {'word_to_analyse':'election', 'model':'w2v'}
+        response = self.app.post('http://localhost:5000/', data=data)
+        #print(response.data)        
+        self.assertEqual(response.status_code,200)
+
+    def test_model_d2v(self):
+        data = {'word_to_analyse':'election', 'model':'d2v'}
         response = self.app.post('http://localhost:5000/', data=data)
         #print(response.data)        
         self.assertEqual(response.status_code,200)
